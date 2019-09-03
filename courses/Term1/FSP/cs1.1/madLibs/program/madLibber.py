acffa97c202d2fac652672a92d952a2ad147f184
Parker M Paisley <ppaisley94@gmail.com>
2019-09-02 23:48:51 -0700

    finished wireFraming, moving to program writing. Took another break, made some oatmeal. Enjoying myself

---
+++ courses/Term1/FSP/cs1.1/madLibs/program/madLibber.py
@@ -0,0 +1,18 @@
+class statementClass:
+
+    statements = {'sentence':0, 'paragraph':1, 'story':2}
+
+    print(statements['sentence'])
+    """establishing outline of class for our program"""
+
+    def __init__(self, name):
+        self.name = name
+
+    def sentence_Lib_1(self, sentence_Lib_1):
+
+        [sentence_Lib_1]
+
+        "Jimmy decided to [0[0]] while he waited for his [0[1]] to make a [0[2]] pi."
+
+
+print('hello world ')
